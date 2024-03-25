94783ef8503344faba45475999f5fa8c11740bd9
drummdurum <147395124+drummdurum@users.noreply.github.com>
2024-03-25 09:44:46 +0100

    først push

---
+++ src/test/java/com/example/musikdatabase/MusikDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musikdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusikDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
